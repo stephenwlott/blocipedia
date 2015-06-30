8663dd415c7ad39b1079909bd53c688aa236b1a2
Stephen Lott <stephenwlott@gmail.com>
2015-06-30 16:32:26 +0000

    Added wiki policy correction

--- app/policies/wiki_policy.rb
+++ app/policies/wiki_policy.rb
@@ -10,24 +10,34 @@ class WikiPolicy < ApplicationPolicy
  
      def resolve
        wikis = []
-       if user.is_admin?
-         wikis = scope.all # if the user is an admin, show them all the wikis
-       elsif user.is_premium?
-         all_wikis = scope.all
-         all_wikis.each do |wiki|
-           if wiki.is_public? || wiki.user == user || wiki.users.include?(user)
-             wikis << wiki # if the user is premium, only show them public wikis, or private wikis they created, or private wikis they are a collaborator on
+       if  user
+         if user.is_admin?
+           wikis = scope.all # if the user is an admin, show them all the wikis
+         elsif user.is_premium?
+           all_wikis = scope.all
+           all_wikis.each do |wiki|
+             if wiki.is_public? || wiki.user == user || wiki.users.include?(user)
+               wikis << wiki # if the user is premium, only show them public wikis, or private wikis they created, or private wikis they are a collaborator on
+             end
+           end
+         else # this is the lowly standard user
+           all_wikis = scope.all
+           wikis = []
+           all_wikis.each do |wiki|
+             if wiki.is_public? || wiki.users.include?(user)
+               wikis << wiki # only show standard users public wikis and private wikis they are a collaborator on
+             end
            end
          end
-       else # this is the lowly standard user
+       else #no user signed in
          all_wikis = scope.all
          wikis = []
          all_wikis.each do |wiki|
-           if wiki.is_public? || wiki.users.include?(user)
-             wikis << wiki # only show standard users public wikis and private wikis they are a collaborator on
+           if wiki.is_public?
+             wikis << wiki # only show non-users public wikis
            end
          end
-       end
+       end  
        wikis # return the wikis array we've built up
      end
    end
